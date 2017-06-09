5f9dd0bf748b981bd42d74b660959f484c65f2ac
David Read <david.read@hackneyworkshop.com>
2017-06-09 11:26:17 +0100

    Cambridge import instrumented and more robust

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
+import time
+
 def transform_polygons_to_multipolygon(polygons):
-    # This method expects the Polygons ins GeoJSON format and will return a
-    # MultiPolygon in GeoJSON format too.
+    '''This method expects the Polygons ins GeoJSON format and will return a
+    MultiPolygon in GeoJSON format too.
+    '''
     if len(polygons) == 0:
         return None
 
@@ -16,3 +19,11 @@ def transform_polygons_to_multipolygon(polygons):
             polygon['geom']['coordinates'][0])
 
     return multipolygon
+
+def print_outcomes_and_rate(outcomes, start_time):
+    total_count = sum([len(rows) for rows in outcomes.values()])
+    rate_per_hour = total_count / (time.time() - start_time) * 60 * 60
+    for outcome, rows in outcomes.items():
+        print('{:,} {} e.g. {}'.format(len(rows), outcome, rows[0]))
+    print('Count: {:,} Rate: {:,.0f}/hour'
+          .format(total_count, round(rate_per_hour, -3)))
